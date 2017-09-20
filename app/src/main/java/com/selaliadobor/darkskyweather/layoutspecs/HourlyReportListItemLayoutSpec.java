a5b796269b1bfe41c68764886504c746593228df
Selali Adobor <selali.adobor@gmail.com>
2017-09-19 23:38:20 -0400

    Add expandable short term view
    Shows short term weather listings and allows showing an increased number when tapped

--- app/src/main/java/com/selaliadobor/darkskyweather/layoutspecs/HourlyReportListItemLayoutSpec.java
+++ app/src/main/java/com/selaliadobor/darkskyweather/layoutspecs/HourlyReportListItemLayoutSpec.java
@@ -2,13 +2,18 @@ package com.selaliadobor.darkskyweather.layoutspecs;
 
 
 import android.graphics.Color;
+import android.view.View;
 
+import com.facebook.litho.ClickEvent;
 import com.facebook.litho.Column;
 import com.facebook.litho.ComponentContext;
 import com.facebook.litho.ComponentLayout;
+import com.facebook.litho.EventHandler;
 import com.facebook.litho.Row;
+import com.facebook.litho.annotations.FromEvent;
 import com.facebook.litho.annotations.LayoutSpec;
 import com.facebook.litho.annotations.OnCreateLayout;
+import com.facebook.litho.annotations.OnEvent;
 import com.facebook.litho.annotations.Prop;
 import com.facebook.litho.annotations.PropDefault;
 import com.facebook.litho.widget.Text;
@@ -26,7 +31,8 @@ public class HourlyReportListItemLayoutSpec {
     static ComponentLayout onCreateLayout(
             ComponentContext c,
             @Prop HourlyReport hourlyReport,
-            @Prop int heightDip) {
+            @Prop int heightDip,
+            @Prop Runnable clickEventHandler) {
         String temperatureString = String.format(Locale.ENGLISH, "%.2f° F", hourlyReport.getTemperature());
         ComponentLayout textColumn = Column.create(c)
                 .marginDip(YogaEdge.LEFT, 16)
@@ -52,6 +58,14 @@ public class HourlyReportListItemLayoutSpec {
                     .fitCenter(true)
                     .buildWithLayout())
                 .child(textColumn)
+                .clickHandler(HourlyReportListItemLayout.onClick(c))
                 .build();
+    }
+    @OnEvent(ClickEvent.class)
+    static void onClick(
+            ComponentContext c,
+            @FromEvent View view,
+            @Prop Runnable clickEventHandler) {
+        clickEventHandler.run();
     }
 }
--- app/src/main/java/com/selaliadobor/darkskyweather/search/SearchFragment.java
+++ app/src/main/java/com/selaliadobor/darkskyweather/search/SearchFragment.java
@@ -4,7 +4,6 @@ package com.selaliadobor.darkskyweather.search;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.OrientationHelper;
-import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,18 +31,21 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import io.realm.Realm;
+import io.realm.RealmResults;
 import io.realm.Sort;
+import rx.functions.Action1;
 import rx.subscriptions.CompositeSubscription;
 
 public class SearchFragment extends Fragment {
     public static final int HOURLY_VIEW_ITEM_HEIGHT_DP = 50;
-    public static final int SHORT_TERM_ITEM_COUNT = 4;
+    public static final int SHORT_TERM_EXPANDED_ITEM_COUNT = 7;
+    public static final int SHORT_TERM_VIEW_EXPANDED_HEIGHT = SHORT_TERM_EXPANDED_ITEM_COUNT * HOURLY_VIEW_ITEM_HEIGHT_DP;
+    public static final int SHORT_TERM_COMPRESSED_ITEM_COUNT = 4;
+    public static final int SHORT_TERM_VIEW_COMPRESSED_HEIGHT = SHORT_TERM_COMPRESSED_ITEM_COUNT * HOURLY_VIEW_ITEM_HEIGHT_DP;
 
     @BindView(R.id.search_short_term_lithoView)
     LithoView shortTermLithoView;
 
-    @BindView(R.id.search_short_term_frame)
-    ViewGroup shortTermViewGroup;
 
     @BindView(R.id.search_zip_code_editText)
     EditText zipCodeEditText;
@@ -51,12 +53,16 @@ public class SearchFragment extends Fragment {
     @BindView(R.id.search_update_zip_button)
     Button updateZipButton;
 
+
     CompositeSubscription lifecycleSubscriptions = new CompositeSubscription();
+    Action1<Boolean> updateRecycler;
+    boolean isExpanded = false;
     private Unbinder unbinder;
 
     public SearchFragment() {
     }
-    public static SearchFragment create(){
+
+    public static SearchFragment create() {
         SearchFragment fragment = new SearchFragment();
         return fragment;
     }
@@ -70,7 +76,7 @@ public class SearchFragment extends Fragment {
     @Override
     public void onPause() {
         super.onPause();
-        if(unbinder != null){
+        if (unbinder != null) {
             unbinder.unbind();
             unbinder = null;
         }
@@ -86,10 +92,10 @@ public class SearchFragment extends Fragment {
         lifecycleSubscriptions.add(
                 RxTextView.textChanges(zipCodeEditText)
                         .subscribe(charSequence -> {
-                            if(charSequence.length() < 5){
+                            if (charSequence.length() < 5) {
                                 updateZipButton.setEnabled(false);
                                 updateZipButton.setText("Invalid Zip");
-                            }else{
+                            } else {
                                 updateZipButton.setEnabled(true);
                                 updateZipButton.setText("Set Zip");
                             }
@@ -99,10 +105,10 @@ public class SearchFragment extends Fragment {
                         .subscribe(aVoid -> {
                             String zipCode = zipCodeEditText.getText().toString();
                             try {
-                                RetrieveWeatherJob.startWeatherRetrievalJob(zipCode,getActivity());
+                                RetrieveWeatherJob.startWeatherRetrievalJob(zipCode, getActivity());
                             } catch (RetrieveWeatherJobSetupException exception) {
                                 Toast
-                                        .makeText(getContext(),"Failed to update zip: "+exception.getMessage(),Toast.LENGTH_LONG)
+                                        .makeText(getContext(), "Failed to update zip: " + exception.getMessage(), Toast.LENGTH_LONG)
                                         .show();
                             }
                         }));
@@ -117,41 +123,93 @@ public class SearchFragment extends Fragment {
         final ComponentContext componentContext = new ComponentContext(getActivity());
 
         final RecyclerBinder recyclerBinder = new RecyclerBinder.Builder()
-                .layoutInfo( new LinearLayoutInfo(getActivity(), OrientationHelper.VERTICAL, false))
+                .layoutInfo(new LinearLayoutInfo(getActivity(), OrientationHelper.VERTICAL, false))
                 .build(componentContext);
 
 
         Date value = new Date();
         lifecycleSubscriptions.add(
                 realm
-                    .where(HourlyReport.class)
-                    .greaterThan("date", value.getTime())
-                    .findAllSorted("date", Sort.ASCENDING)
-                    .asObservable()
-                    .subscribe(hourlyReports -> {
-                        int itemCount = Math.min(SHORT_TERM_ITEM_COUNT,hourlyReports.size());
-                        for (int i = 0; i < itemCount; i++) {
-                            HourlyReport hourlyReport = realm.copyFromRealm(hourlyReports.get(i));
-                            Component<HourlyReportListItemLayout> listItem = HourlyReportListItemLayout.create(componentContext)
-                                    .heightDip(HOURLY_VIEW_ITEM_HEIGHT_DP)
-                                    .hourlyReport(hourlyReport).build();
-                            if(recyclerBinder.isValidPosition(i)){
-                                recyclerBinder.updateItemAt(i,listItem);
-                            }else{
-                                recyclerBinder.insertItemAt(i,listItem);
-                            }
-
-                            if(hourlyReports.size() < SHORT_TERM_ITEM_COUNT){
-                                recyclerBinder.removeRangeAt(hourlyReports.size() - 1,SHORT_TERM_ITEM_COUNT-hourlyReports.size());
-                            }
-
-                        }
-                    })
+                        .where(HourlyReport.class)
+                        .greaterThan("date", value.getTime())
+                        .findAllSorted("date", Sort.ASCENDING)
+                        .asObservable()
+                        .subscribe(hourlyReports -> {
+                            listNewReports(realm, componentContext, recyclerBinder, hourlyReports);
+                        })
         );
+
 
         final Component<Recycler> recyclerComponent = Recycler.create(componentContext)
                 .binder(recyclerBinder)
                 .build();
+
         shortTermLithoView.setComponent(recyclerComponent);
+
+    }
+
+    private void listNewReports(Realm realm, ComponentContext componentContext, RecyclerBinder recyclerBinder, RealmResults<HourlyReport> hourlyReports) {
+        updateRecycler = (isExpanded) -> {
+            int itemCount = isExpanded ?
+                    hourlyReports.size() :
+                    Math.min(SHORT_TERM_COMPRESSED_ITEM_COUNT, hourlyReports.size());
+            if (itemCount < recyclerBinder.getItemCount() && recyclerBinder.getItemCount() > 0) {
+                recyclerBinder.removeRangeAt(Math.max(0, itemCount - 1), Math.max(0,  recyclerBinder.getItemCount() - itemCount));
+            }
+            for (int i = 0; i < itemCount; i++) {
+                HourlyReport hourlyReport = realm.copyFromRealm(hourlyReports.get(i));
+                Component<HourlyReportListItemLayout> listItem = HourlyReportListItemLayout.create(componentContext)
+                        .heightDip(HOURLY_VIEW_ITEM_HEIGHT_DP)
+                        .clickEventHandler(this::toggleVisibility)
+                        .hourlyReport(hourlyReport).build();
+                if (recyclerBinder.isValidPosition(i)) {
+                    recyclerBinder.updateItemAt(i, listItem);
+                } else {
+                    recyclerBinder.insertItemAt(i, listItem);
+                }
+
+
+            }
+        };
+        updateRecycler.call(isExpanded);
+    }
+
+    void toggleVisibility() {
+        if (updateRecycler == null) {
+            return;
+        }
+
+        isExpanded = !isExpanded;
+
+
+        shortTermLithoView.animate()
+                .scaleY(1f)
+                .setUpdateListener((value) -> {
+                    final float scale = getResources().getDisplayMetrics().density;
+                    float startingHeight = isExpanded ?
+                            SHORT_TERM_VIEW_COMPRESSED_HEIGHT :
+                            SHORT_TERM_VIEW_EXPANDED_HEIGHT;
+                    float targetHeight = isExpanded ?
+                            SHORT_TERM_VIEW_EXPANDED_HEIGHT :
+                            SHORT_TERM_VIEW_COMPRESSED_HEIGHT;
+                    float delta = targetHeight - startingHeight;
+
+                    int currentDelta = (int) (delta * (1.0 - value.getAnimatedFraction()));
+
+                    shortTermLithoView.getLayoutParams().height = (int) (scale * (targetHeight - currentDelta));
+                    shortTermLithoView.requestLayout();
+                })
+                //If the list is expanding, immediately add new items, otherwise wait until animation finishes
+                .withStartAction(() -> {
+                    if (isExpanded) {
+                        updateRecycler.call(isExpanded);
+                    }
+                })
+                .withEndAction(() -> {
+                    if (!isExpanded) {
+                        updateRecycler.call(isExpanded);
+                    }
+                })
+                .setDuration(500);
     }
 }
